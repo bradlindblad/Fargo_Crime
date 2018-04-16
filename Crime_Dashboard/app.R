2e49588fc2b0142ea07e5d3533627f6210bdca99
bradlindblad <bradley.lindblad@gmail.com>
2018-04-15 21:53:54 -0500

    worked on turning Fargo Crime into a more dashboard-esque dashboard.

--- Crime_Dashboard/app.R
+++ Crime_Dashboard/app.R
@@ -1,49 +1,181 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(shinydashboard)
+library(leaflet)
+library(tidyverse)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
+##################
+#### D A T A #####
+##################
+
+mydata <- read.csv("crime.csv", header = T)
+mydata$Date.Time <- lubridate::mdy(mydata$Date.Time)
+mydata$Time <- lubridate::mdy_hms(mydata$Time)
+
+##################
+##### F N S ######
+##################
+
+leafPlot <- function(x = type){
+  
+  
+  
+  mydata <- filter(mydata, Call.Type == x)
+  
+  
+  
+  leaflet() %>% setView(lng = -96.85, lat = 46.85, zoom = 11) %>% addProviderTiles(providers$OpenStreetMap.DE) %>%
+    
+    leaflet::addMiniMap(position = "topleft") %>%
+    
+    leaflet::addCircleMarkers(
+      lng = mydata$Lon,
+      lat = mydata$Lat,
+      label = mydata$Description,
+      opacity = 0.2,
+      stroke = F,
+      color = "red")
+  
+  
+  
+  
+  
+}
+
+plotMonth <- function(x = "type"){
+  
+  # Order data by Months
+  mydata <- mydata
+  mydata$Month <- factor(mydata$Month, c("Jan", "Feb", "Mar", "Apr", "May",
+                                         "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"))
+  mydata <- dplyr::filter(mydata, Call.Type == x)
+  
+  ggplot(mydata,aes(x= mydata$Month, fill=..count..))+
+    scale_fill_gradient(low = "green", high = "red")+
+    geom_bar(width=0.7) +
+    xlab("Month of Crime") +
+    ylab("Number of Calls") +
+    ggtitle("Number of Crimes by Month") +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  
+}
+
+plotWeekday <- function(x = "type"){
+  
+  # Order data by Months
+  mydata <- mydata
+  mydata$Weekday <- factor(mydata$Weekday, c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+                                             "Friday", "Saturday"))
+  mydata <- dplyr::filter(mydata, Call.Type == x)
+  
+  ggplot(mydata,aes(x= mydata$Weekday, fill=..count..))+
+    scale_fill_gradient(low = "green", high = "red")+
+    geom_bar(width=0.7) +
+    xlab("Month of Crime") +
+    ylab("Number of Calls") +
+    ggtitle("Number of Crimes by Weekday") +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))
+}
+
+
+##################
+###### U I #######
+##################
+
+ui <- dashboardPage(
+  
+
+  ## H E A D E R
+  dashboardHeader(title = "FARGO CRIME"),
+  
+  
+  ## S I D E B A R
+  dashboardSidebar(
+    
+      selectInput(inputId = "choose.crime",
+                  label = "Choose a Crime to Explore",
+                  choices = unique(mydata$Call.Type),
+                  selected = "Impaired Person"),
       
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
+      h6("Data Compiled by Brad Lindblad, MBA, Fargo, North Dakota.")
+  ),
+  
+  
+  ## B O D Y
+  dashboardBody(
+    
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+    ),
+    
+    fluidRow(
+      column(width = 9,
+             
+             
+      #tags$style(type = "text/css", ".box-body {height:80vh}"),
+      
+      
+      box(leafletOutput("choose.crime", 
+                        width = "100%",
+                        height = "740px"),
+          width = "100%",
+          height = "780px"
       )
-   )
+      ),
+      column(width = 3,
+             
+             box(
+             plotOutput("weekday.bar",
+                        height = "280px",
+                        width = "100%"),
+             height = "300px",
+             width = "300px"
+             ),
+             
+             
+             box(
+               plotOutput("month.bar",
+                          height = "280px",
+                          width = "100%"),
+               height = "300px",
+               width = "300px"
+               
+             )
+             
+      )
+  )
+  )
 )
 
-# Define server logic required to draw a histogram
+
+##################
+## S E R V E R ###
+##################
+
+
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+
+  
+  output$choose.crime <- renderLeaflet({
+    
+    leafPlot(input$choose.crime)
+    
+  })
+  
+  output$weekday.bar <- renderPlot({
+    
+    
+    plotWeekday(input$choose.crime)
+    
+  })
+  
+  output$month.bar <- renderPlot({
+    
+    plotMonth(input$choose.crime)
+    
+  })
+  
 }
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
